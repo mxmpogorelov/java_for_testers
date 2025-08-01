0b06856c3bdcaf29f1cbf4d534872dbc2da2ab77
mxmpogorelov <mxmpogorelov@gmail.com>
2025-08-01 18:27:56 +0300

    Тренеруюсь

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -2,14 +2,15 @@ import com.sun.security.jgss.GSSUtil;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello, world");
+        System.out.println("Hello, world");
 
-        System.out.println(2 +2);
-        System.out.println(2 -2);
-        System.out.println(2 *2);
+        System.out.println(2 + 2);
+        System.out.println(2 - 2);
+        System.out.println(2 * 2);
         System.out.println(2 / 2);
         System.out.println((2 + 2) * 6);
 
-        System.out.println("Hello," + "world");
-        System.out.println("2 +2 =" + (2 + 2));
+        System.out.println("Hi," + "world");
+        System.out.println("2 * 2 = " + (2 + 2));
+        System.out.println("Это всем известно в целом мире");
     }}
