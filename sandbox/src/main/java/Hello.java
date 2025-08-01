5f4e6ce5332da0448a07d9ebeefb6e4d5c4c5503
mxmpogorelov <mxmpogorelov@gmail.com>
2025-08-01 18:22:28 +0300

    Тренировался

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,4 +1,15 @@
+import com.sun.security.jgss.GSSUtil;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello, world");
+
+        System.out.println(2 +2);
+        System.out.println(2 -2);
+        System.out.println(2 *2);
+        System.out.println(2 / 2);
+        System.out.println((2 + 2) * 6);
+
+        System.out.println("Hello," + "world");
+        System.out.println("2 +2 =" + (2 + 2));
     }}
