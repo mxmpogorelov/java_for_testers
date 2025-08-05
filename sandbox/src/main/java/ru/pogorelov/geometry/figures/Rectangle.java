e1ce08cd8f05064443a88253222b5cfe27707673
mxmpogorelov <mxmpogorelov@gmail.com>
2025-08-05 13:41:33 +0300

    Выполнена структуризация кода

--- sandbox/src/main/java/ru/pogorelov/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/pogorelov/geometry/figures/Rectangle.java
@@ -2,7 +2,8 @@ package ru.pogorelov.geometry.figures;
 
 public class Rectangle {
     public static void printRectangleArea(double a, double b) {
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + RectangleArea(a, b));
+        var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a, b, RectangleArea(a, b));
+        System.out.println(text);
     }
 
     private static double RectangleArea(double a, double b) {
