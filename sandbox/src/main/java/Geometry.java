46b014d27d98bb667df21ee8d57fd5cbeb7546ca
mxmpogorelov <mxmpogorelov@gmail.com>
2025-08-05 12:01:34 +0300

    Площадь квадрата

---
+++ sandbox/src/main/java/Geometry.java
@@ -0,0 +1,6 @@
+public class Geometry {
+    public static void main(String[] args) {
+        var side = 7.0;
+        System.out.println("Площадь квадрата со стороной " + side + " = " + (side * side));
+    }
+}
