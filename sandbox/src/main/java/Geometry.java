73615ea35c2ea60d12a0b2aadb74459fe3291cf7
mxmpogorelov <mxmpogorelov@gmail.com>
2025-08-05 12:58:16 +0300

    Эксперименты с функциями

--- sandbox/src/main/java/Geometry.java
+++ sandbox/src/main/java/Geometry.java
@@ -1,6 +1,26 @@
 public class Geometry {
     public static void main(String[] args) {
-        var side = 7.0;
-        System.out.println("Площадь квадрата со стороной " + side + " = " + (side * side));
+        printSquareArea(0.7);
+        printSquareArea(0.5);
+        printSquareArea(0.3);
+
+        printRectangleArea(3.0 , 5.0);
+        printRectangleArea(7.0 , 9.0);
+    }
+
+    private static void printRectangleArea(double a, double b) {
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + RectangleArea(a, b));
+    }
+
+    private static double RectangleArea(double a, double b) {
+        return a * b;
+    }
+
+    static void printSquareArea(double a) {
+        System.out.println("Площадь квадрата со стороной " + a + " = " + SquareArea(a));
+    }
+
+    private static double SquareArea(double a) {
+        return a * a;
     }
 }
